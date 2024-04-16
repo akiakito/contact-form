16831b4378bb7f2601673a9ea02ff2a291761f2d
aki <akito.t.12.04@gmail.com>
2024-04-16 11:58:21 +0900

    three

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
     public function index()
-  {
+{
     return view('index');
-  }
- public function confirm(Request $request)
+}
+public function confirm(Request $request)
     {
-         $contact = $request->only(['name', 'email', 'tel', 'content']);
-         return $contact;
+        $contact = $request->only(['name', 'email', 'tel', 'content']);
+        return view('confirm', compact('contact'));
+        Contact::create($contact);
+        return view('thanks');
+    }
+public function store()
+    {
+    $contact = $request->only(['name', 'email', 'tel', 'content']);
+
     }
 }
